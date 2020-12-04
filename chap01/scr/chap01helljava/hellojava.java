24177544d71260a3d78b311ba2996b71e4943dbd
newkayak12 <newkayak12@gmail.com>
2020-12-05 00:05:53 +0900

    start java

---
+++ chap01/scr/chap01helljava/hellojava.java
@@ -0,0 +1,12 @@
+package chap01helljava;
+
+public class hellojava {
+	
+	public static void main(String[] args) {
+
+		System.out.println("hello java");
+		}
+		
+	}
+
+
---
+++ chap02/src/sec01/exam01_variable/variableExample.java
@@ -0,0 +1,12 @@
+package sec01.exam01_variable;
+
+public class variableExample {
+	public static void main(String[] args) {
+		int value = 10;// value¿¡ 10 ÇÒ´ç
+		int result = value + 20; //result= value+20
+		
+		System.out.println(result); //print.out
+		
+				
+	}
+}
