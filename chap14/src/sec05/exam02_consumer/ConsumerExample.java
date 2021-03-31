22aeae95f3fc475eb31d5acc898b4fd5ee43beec
newkayak12 <newkayak12@gmail.com>
2021-03-31 19:45:03 +0900

    javastart

--- chap14/src/sec05/exam02_consumer/ConsumerExample.java
+++ chap14/src/sec05/exam02_consumer/ConsumerExample.java
@@ -8,6 +8,20 @@ public class ConsumerExample {
 		Consumer<String> consumer = t -> System.out.println(t+11);
 		consumer.accept("java ");
 //		소비...
+		
+		
+		BiConsumer<String, String> biConsumer = (t, u) -> System.out.println(t+u);
+		biConsumer.accept("java ","Script");
+		
+		
+		DoubleConsumer dCon = d -> System.out.println("java "+ d);
+		dCon.accept(15.2);
+		
+		ObjIntConsumer<String> objIntCon = (t,i) -> System.out.println(t + i);
+		
+		objIntCon.accept("Java ", 16);
+		
+		
 	}
 
 }
